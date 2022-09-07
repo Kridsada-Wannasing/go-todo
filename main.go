1e103edcf38e4335747a83e1f5e4b47fe24a319b
Kridsada-Wannasing <kridsada_aex002@hotmail.com>
2022-09-07 21:43:08 +0700

    feat: jwt auth

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 
+	"github.com/Kridsada-Wannasing/todo/auth"
 	"github.com/Kridsada-Wannasing/todo/todo"
 )
 
@@ -22,6 +23,8 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	r.GET("/tokenz", auth.AccessToken)
 
 	handler := todo.NewTodoHandler(db)
 	r.POST("/todos", handler.NewTask)
