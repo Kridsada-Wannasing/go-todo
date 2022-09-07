bf50f3c390ad3dbdfc3ef3f522beb7e6449b0647
Kridsada-Wannasing <kridsada_aex002@hotmail.com>
2022-09-07 21:11:30 +0700

    feat: add handler

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+
+	"github.com/Kridsada-Wannasing/todo/todo"
 )
 
 func main() {
@@ -12,12 +14,17 @@ func main() {
 		panic("failed to connect database")
 	}
 
+	db.AutoMigrate(&todo.Todo{})
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	handler := todo.NewTodoHandler(db)
+	r.POST("/todos", handler.NewTask)
 
 	r.Run()
 }
